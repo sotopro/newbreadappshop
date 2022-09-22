a22648f1f06eec4e3afb65afb76910d2abceccd3
sotopro <dasg.1993@gmail.com>
2022-09-21 20:28:46 -0500

    [feature] - add button to navigate

--- src/screens/categories/index.js
+++ src/screens/categories/index.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { styles } from "./styles";
 
-const Categories = () => {
+const Categories = ({ navigation }) => {
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Categories</Text>
+            <Button 
+                title="Go to Products"
+                onPress={() => navigation.navigate("Products")}
+            />
         </View>
     )
 };
--- src/screens/products/index.js
+++ src/screens/products/index.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { styles } from "./styles";
 
-const Products = () => {
+const Products = ({ navigation  }) => {
     return (
         <View style={styles.container}>
             <Text>Products</Text>
+            <Button 
+                title="Go to Product"
+                onPress={() => navigation.navigate("Product")}
+            />
         </View>
     )
 };
