4538d1f4d8e79bcd3e568b4ecab0206c6d787f25
sotopro <dasg.1993@gmail.com>
2022-09-26 20:27:09 -0500

    [feature] - add product detail

--- src/screens/product/index.js
+++ src/screens/product/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { styles } from "./styles";
+import { products } from "../../constants/data";
 
-const Product = () => {
+const Product = ({ navigation, route }) => {
+
+    const { productId } = route.params;
+    const product = products.find(product => product.id === productId);
     return (
         <View style={styles.container}>
-            <Text>Product</Text>
+            <Text>id: {product.id}</Text>
+            <Text>{product.title}</Text>
+            <Text>{product.description}</Text>
+            <Text>${product.price}</Text>
+            <Text>weight: {product.weight}</Text>
         </View>
     )
 };
