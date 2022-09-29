26e593a525dd9f238f800773fb68c544579c54bb
sotopro <dasg.1993@gmail.com>
2022-09-28 20:02:44 -0500

    [feature] - create cart item & cart view

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as CategoryItem } from './category-item';
 export { default as ProductItem } from './product-item';
+export { default as CartItem } from './cart-item';
--- src/screens/cart/index.js
+++ src/screens/cart/index.js
@@ -1,11 +1,40 @@
 import React from "react";
-import { View, Text} from "react-native";
+import { View, FlatList, TouchableOpacity, Text} from "react-native";
 import { styles } from "./styles";
+import { cart } from "../../constants/data";
+import CartItem from "../../components/cart-item";
 
 const Cart = ({navigation}) => {
+    const total = 1400;
+    
+    const onDelete = (id) => {
+        console.warn(id);
+    }
+
+    const renderItem = ({item}) => <CartItem item={item} onDelete={onDelete} />
+
     return (
         <View style={styles.container}>
-            <Text>Cart</Text>
+            <View style={styles.containerList}>
+           <FlatList 
+                data={cart}
+                renderItem={renderItem}
+                style={styles.containerList}
+                keyExtractor={item => item.id.toString()}
+            />
+            </View>
+            <View style={styles.footer}>
+                <TouchableOpacity 
+                    style={styles.buttonConfirm}
+                    onPress={() => null}
+                >
+                    <Text style={styles.textButtonConfirm}>Confirm</Text>
+                    <View style={styles.totalContainer}>
+                        <Text style={styles.totalTitle}>Total: </Text>
+                        <Text style={styles.total}>${total}</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
         </View>
     )
 };
