6f6a99b25966cda51c4d291939b56d9d49be29f8
sotopro <dasg.1993@gmail.com>
2022-10-10 20:39:06 -0500

    [feature] - create form reducer & create validation & action on input change

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,4 +1,4 @@
-export const API_KEY = 'AIzaSyDL5JitfPJ5Q_g_UzAF30mi4UN0hIXg4-g'
-export const URL_API = 'https://newbreadappshop-7581a-default-rtdb.firebaseio.com/';
+export const API_KEY = ''
+export const URL_API = '';
 export const URL_AUTH_SIGN_IN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
 export const URL_AUTH_SIGN_UP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
